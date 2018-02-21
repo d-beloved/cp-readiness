8fc3fe53b45f98d3ae2eb1145d8826fc291122e9
d-beloved <morayodeji@gmail.com>
2018-02-21 23:08:31 +0100

    Setting up the website server file to use express routers

---
+++ Server.js
@@ -0,0 +1,33 @@
+import express from "express";
+const app = express();
+const router = express.Router();
+const path = `${__dirname}/views/`;// points to the current working directory housing all the pages to be served
+
+// the middle layer that prints the type of HTTP request
+// a particular route is referring to
+router.use((req, res, next) => {
+  console.log(`/${req.method}`);
+  next();
+});
+
+router.get("/",(req, res) => {
+  res.sendFile(`${path}index.html`);
+});
+
+router.get("/about",(req, res) => {
+  res.sendFile(`${path}about.html`);
+});
+
+router.get("/contact",(req, res) => {
+  res.sendFile(`${path}contact.html`);
+});
+
+app.use("/",router);
+
+app.use("*",(req, res) => {
+  res.sendFile(`${path}404.html`);
+});
+
+app.listen(3000,() => {
+  console.log("Live at Port 3000");
+})
